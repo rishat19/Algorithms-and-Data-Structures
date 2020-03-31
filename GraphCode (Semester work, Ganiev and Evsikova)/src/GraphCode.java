386cac73c23597a188a462f6d06ee7f502634407
rishat19 <57725670+rishat19@users.noreply.github.com>
2020-04-01 01:10:10 +0300

    All methods added

--- GraphCode (Semester work, Ganiev and Evsikova)/src/GraphCode.java
+++ GraphCode (Semester work, Ganiev and Evsikova)/src/GraphCode.java
@@ -11,15 +11,9 @@ public class GraphCode {
     private class Edge {
         private int beginningOfEdge;
         private int endOfEdge;
-        public Edge(int beginningOfEdge, int endOfEdge) {
+        private Edge(int beginningOfEdge, int endOfEdge) {
             this.beginningOfEdge = beginningOfEdge;
             this.endOfEdge = endOfEdge;
-        }
-        public int getBeginningOfEdge() {
-            return beginningOfEdge;
-        }
-        public int getEndOfEdge() {
-            return endOfEdge;
         }
         @Override
         public boolean equals(Object o) {
@@ -33,6 +27,10 @@ public class GraphCode {
         public int hashCode() {
             return Objects.hash(beginningOfEdge, endOfEdge);
         }
+        @Override
+        public String toString() {
+            return "(" + beginningOfEdge + " -> " + endOfEdge + ")";
+        }
     }
 
     public GraphCode() {
@@ -43,6 +41,143 @@ public class GraphCode {
     //конструктор  построение графа по матрице инцидентности
     public GraphCode(int[][] incidenceMatrix) throws IllegalArgumentException, NullPointerException {
         this();
+        readIncidenceMatrix(incidenceMatrix);
+    }
+
+    // построение матрицы инцидентности
+    public int[][] getIncidenceMatrix() {
+        if (maxVertexNumber == 0) {
+            return null;
+        }
+        int[][] incidenceMatrix = new int[maxVertexNumber][edges.size()];
+        for (int i = 0; i < edges.size(); i++) {
+            int beginningOfEdge = edges.get(i).beginningOfEdge;
+            int endOfEdge = edges.get(i).endOfEdge;
+            if (beginningOfEdge == endOfEdge) {
+                incidenceMatrix[beginningOfEdge - 1][i] = 2;
+            }
+            else {
+                incidenceMatrix[beginningOfEdge - 1][i] = 1;
+                incidenceMatrix[endOfEdge - 1][i] = -1;
+            }
+        }
+        return incidenceMatrix;
+    }
+
+    //вставка некоторого ребра (i,j)
+    public void insert(int beginningOfEdge, int endOfEdge) throws IllegalArgumentException {
+        if (beginningOfEdge < 1 || endOfEdge < 1 ||
+                beginningOfEdge > maxVertexNumber + 2 || endOfEdge > maxVertexNumber + 2 ||
+                (beginningOfEdge == maxVertexNumber + 2 && endOfEdge == maxVertexNumber + 2)) {
+            if (maxVertexNumber == 0) {
+                throw new IllegalArgumentException("The graph has no vertices yet. " +
+                        "You can insert only the edges: 1-1, 1-2 or 2-1");
+            }
+            else {
+                throw new IllegalArgumentException("The graph has vertices with numbers from " + 1 + " to " +
+                        maxVertexNumber + ". You can insert edges connecting vertices with numbers from 1 to " +
+                        (maxVertexNumber + 1) + " or edges: " + (maxVertexNumber + 1) + "-" + (maxVertexNumber + 2) +
+                        ", " + (maxVertexNumber + 2) + "-" + (maxVertexNumber + 1));
+            }
+        }
+        else {
+            if (Integer.max(beginningOfEdge, endOfEdge) > maxVertexNumber) {
+                maxVertexNumber = Integer.max(beginningOfEdge, endOfEdge);
+            }
+            edges.add(new Edge(beginningOfEdge, endOfEdge));
+        }
+    }
+
+    // удаление ребра (i,j) из списка
+    public void delete(int beginningOfEdge, int endOfEdge) {
+        if (beginningOfEdge > 0 && endOfEdge > 0 &&
+                beginningOfEdge <= maxVertexNumber && endOfEdge <= maxVertexNumber) {
+            edges.remove(new Edge(beginningOfEdge, endOfEdge));
+        }
+    }
+
+    // возврат списка ребер, инцидентных вершине i
+    public GraphCode getEdgesWithNode(int i) {
+        GraphCode graph = new GraphCode();
+        if (i > 0 && i <= maxVertexNumber) {
+            for (Edge edge : edges) {
+                if (i == edge.beginningOfEdge || i == edge.endOfEdge) {
+                    graph.insert(edge.beginningOfEdge, edge.endOfEdge);
+                }
+            }
+        }
+        return graph;
+    }
+
+    //модифицировать список в связи с удалением вершины i из графа (удалить все ребра, инцидентные удаленной вершине)
+    public void modify(int i) {
+        if (i > 0 && i <= maxVertexNumber) {
+            for (int j = edges.size() - 1; j >= 0; j--) {
+                if (i == edges.get(j).beginningOfEdge || i == edges.get(j).endOfEdge) {
+                    edges.remove(j);
+                }
+                else {
+                    if (edges.get(j).beginningOfEdge > i) {
+                        edges.get(j).beginningOfEdge--;
+                    }
+                    if (edges.get(j).endOfEdge > i) {
+                        edges.get(j).endOfEdge--;
+                    }
+                }
+            }
+            maxVertexNumber--;
+        }
+    }
+
+    //возвратить список вершин, полустепень исхода которых более m.
+    public ArrayList<Integer> outdegreeShow(int m) {
+        if (edges.isEmpty()) {
+            return null;
+        }
+        ArrayList<Integer> vertices = new ArrayList<>();
+        if (m < 0) {
+            for (int i = 1; i <= maxVertexNumber; i++) {
+                vertices.add(i);
+            }
+            return vertices;
+        }
+        int[] outdegrees = new int[maxVertexNumber];
+        for (Edge edge : edges) {
+            outdegrees[edge.beginningOfEdge - 1]++;
+        }
+        for (int i = 0; i < maxVertexNumber; i++) {
+            if (outdegrees[i] > m) {
+                vertices.add(i + 1);
+            }
+        }
+        return vertices;
+    }
+
+    //возвратить список вершин, полустепень захода которых более m.
+    public ArrayList<Integer> indegreeShow(int m) {
+        if (edges.isEmpty()) {
+            return null;
+        }
+        ArrayList<Integer> vertices = new ArrayList<>();
+        if (m < 0) {
+            for (int i = 1; i <= maxVertexNumber; i++) {
+                vertices.add(i);
+            }
+            return vertices;
+        }
+        int[] indegrees = new int[maxVertexNumber];
+        for (Edge edge : edges) {
+            indegrees[edge.endOfEdge - 1]++;
+        }
+        for (int i = 0; i < maxVertexNumber; i++) {
+            if (indegrees[i] > m) {
+                vertices.add(i + 1);
+            }
+        }
+        return vertices;
+    }
+
+    private void readIncidenceMatrix(int[][] incidenceMatrix) throws IllegalArgumentException, NullPointerException {
         if (incidenceMatrix != null) {
             int numberOfVertices = incidenceMatrix.length;
             int numberOfEdges = incidenceMatrix[0].length;
@@ -94,68 +229,57 @@ public class GraphCode {
         }
     }
 
-    // построение матрицы инцидентности
-    public int[][] getIncidenceMatrix() {
-        if (maxVertexNumber == 0) {
-            return null;
-        }
-        int[][] incidenceMatrix = new int[maxVertexNumber][edges.size()];
-        for (int i = 0; i < edges.size(); i++) {
-            int beginningOfEdge = edges.get(i).getBeginningOfEdge();
-            int endOfEdge = edges.get(i).getEndOfEdge();
-            if (beginningOfEdge == endOfEdge) {
-                incidenceMatrix[beginningOfEdge - 1][i] = 2;
-                incidenceMatrix[endOfEdge - 1][i] = 2;
+    public void printIncidenceMatrix() {
+        int[][] incidenceMatrix = this.getIncidenceMatrix();
+        for (int i = 0; i < maxVertexNumber; i++) {
+            for (int j = 0; j < edges.size(); j++) {
+                System.out.printf("%" + 2 + "d", incidenceMatrix[i][j]);
+                System.out.print(" ");
             }
-            else {
-                incidenceMatrix[beginningOfEdge - 1][i] = 1;
-                incidenceMatrix[endOfEdge - 1][i] = -1;
+            System.out.println();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GraphCode graphCode = (GraphCode) o;
+        if (maxVertexNumber == graphCode.maxVertexNumber && edges.size() == graphCode.edges.size()) {
+            boolean[] flags = new boolean[edges.size()];
+            for (int i = 0; i < edges.size(); i++) {
+                for (int j = 0; j < edges.size(); j++) {
+                    if (edges.get(i).equals(graphCode.edges.get(j)) && !flags[j]) {
+                        flags[j] = true;
+                        break;
+                    }
+                }
             }
-        }
-        return incidenceMatrix;
-    }
-
-    //вставка некоторого ребра (i,j)
-    public void insert(int beginningOfEdge, int endOfEdge) throws IllegalArgumentException {
-        if (beginningOfEdge < 1 || endOfEdge < 1 ||
-                beginningOfEdge > maxVertexNumber + 2 || endOfEdge > maxVertexNumber + 2 ||
-                (beginningOfEdge == maxVertexNumber + 2 && endOfEdge == maxVertexNumber + 2)) {
-            if (maxVertexNumber == 0) {
-                throw new IllegalArgumentException("The graph has no vertices yet. " +
-                        "You can insert only the edges: 1-1, 1-2 or 2-1");
+            for (int i = 0; i < edges.size(); i++) {
+                if (!flags[i]) {
+                    return false;
+                }
             }
-            else {
-                throw new IllegalArgumentException("The graph has vertices with numbers from " + 1 + " to " +
-                        maxVertexNumber + ". You can insert edges connecting vertices with numbers from 1 to " +
-                        (maxVertexNumber + 1) + " or edges: " + (maxVertexNumber + 1) + "-" + (maxVertexNumber + 2) +
-                        ", " + (maxVertexNumber + 2) + "-" + (maxVertexNumber + 1));
-            }
+            return true;
         }
-        else {
-            edges.add(new Edge(beginningOfEdge, endOfEdge));
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(edges, maxVertexNumber);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("Graph edges = {");
+        for (int i = 0; i < edges.size() - 1; i++) {
+            str.append(edges.get(i));
+            str.append(", ");
         }
-    }
-
-    // удаление ребра (i,j) из списка
-    public void delete(int beginningOfEdge, int endOfEdge) {
-        if (beginningOfEdge > 0 && endOfEdge > 0 && beginningOfEdge <= maxVertexNumber && endOfEdge <= maxVertexNumber) {
-            edges.remove(new Edge(beginningOfEdge, endOfEdge));
-        }
-    }
-
-    // возврат списка ребер, инцидентных вершине i
-    public GraphCode getEdgesWithNode(int i) {
-        return null;
-    }
-
-    //модифицировать список в связи с удалением вершины i из графа (удалить все ребра, инцидентные удаленной вершине)
-    public void modify(int i) {
-
-    }
-
-    //возвратить список вершин, степень инцидентности которых более m.
-    public ArrayList<Integer> show(int m) {
-        return null;
+        str.append(edges.get(edges.size()- 1));
+        str.append('}');
+        return str.toString();
     }
 
 }
--- GraphCode (Semester work, Ganiev and Evsikova)/src/Main.java
+++ GraphCode (Semester work, Ganiev and Evsikova)/src/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[][] m = new int[3][];
-        m[0] = new int[3];
-        //m[1] = new int[3];
-        m[1] = new int[3];
-        m[2] = new int[3];
+        int[][] m = new int[3][4];
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
                 m[i][j] = scanner.nextInt();
             }
         }
         GraphCode graph = new GraphCode(m);
+        graph.insert(4, 1);
+        graph.insert(2, 4);
+        graph.insert(5, 5);
+        graph.delete(3, 2);
+        graph.modify(4);
+        graph.modify(4);
+        graph.printIncidenceMatrix();
     }
 }
